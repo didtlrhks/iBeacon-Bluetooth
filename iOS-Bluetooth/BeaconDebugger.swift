a18cc3bbe58cac996d7d1c15748fa444c0143eaf
sikwan <yysskk99999@naver.com>
2024-02-15 22:25:25 +0900

    loop miss

--- iOS-Bluetooth/BeaconDebugger.swift
+++ iOS-Bluetooth/BeaconDebugger.swift
@@ -22,12 +22,12 @@ import SwiftUI
 class BeaconDebugger: BeaconBase {
     @Published var currentBeacon: CLBeacon? = nil
     @Published var beaconInfo: [String: [BeaconHistoryItem]] = [:]
-    
-    // 비콘의 마지막 감지 시간을 추적하는 딕셔너리
     var lastSeenBeacons: [String: Date] = [:]
+        var disconnectTimers: [String: Timer] = [:]
 
-    // 비콘 연결 끊김을 확인하는 타이머를 관리하는 딕셔너리
-    var disconnectTimers: [String: Timer] = [:]
+        // 비콘의 연결 상태를 추적하는 딕셔너리
+        var beaconConnectedStatus: [String: Bool] = [:]
+   
 
     
 //    override func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
@@ -36,25 +36,33 @@ class BeaconDebugger: BeaconBase {
 //        }
 //    }
     override func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        super.locationManager(manager, didRangeBeacons: beacons, in: region)
         
-                if beacons.count > 0 {
-                    updateDistance(beacons)
-                }
-        
-        for beacon in beacons {
-            let beaconKey = "\(beacon.proximityUUID.uuidString)-\(beacon.major)-\(beacon.minor)"
-            lastSeenBeacons[beaconKey] = Date()
-
-           
-            disconnectTimers[beaconKey]?.invalidate()
-            disconnectTimers[beaconKey] = nil
-
-            // 새 타이머 설정
-            disconnectTimers[beaconKey] = Timer.scheduledTimer(withTimeInterval: 60, repeats: false) { [weak self] _ in
-                self?.notifyServerAboutBeaconDisconnect(beaconKey: beaconKey)
-                self?.disconnectTimers[beaconKey] = nil
+        if beacons.isEmpty {
+            for beaconKey in lastSeenBeacons.keys {
+                handleDisconnectedBeacon(beaconKey: beaconKey)
             }
+        }else {
+            if beacons.count > 0 {
+                updateDistance(beacons)
+            }
+    
+    for beacon in beacons {
+        let beaconKey = "\(beacon.proximityUUID.uuidString)-\(beacon.major)-\(beacon.minor)"
+        lastSeenBeacons[beaconKey] = Date()
+
+       
+        disconnectTimers[beaconKey]?.invalidate()
+        disconnectTimers[beaconKey] = nil
+
+        // 새 타이머 설정
+        disconnectTimers[beaconKey] = Timer.scheduledTimer(withTimeInterval: 60, repeats: false) { [weak self] _ in
+            self?.notifyServerAboutBeaconDisconnect(beaconKey: beaconKey)
+            self?.disconnectTimers[beaconKey] = nil
         }
+    }
+        }
+          
     }
     func notifyServerAboutBeaconDisconnect(beaconKey: String) {
         // 여기에 비콘 연결 끊김 상태를 서버에 알리는 코드를 추가
@@ -62,6 +70,38 @@ class BeaconDebugger: BeaconBase {
         ServerCommunicator.sendBeaconDataToServer() // 메시지 내용을 적절히 조정해야 함
     }
     
+    func handleConnectedBeacon(beacon: CLBeacon, beaconKey: String) {
+            // 연결 상태 업데이트 및 "연결됨" 메시지 전송 로직
+            if lastSeenBeacons[beaconKey] == nil || beaconConnectedStatus[beaconKey] == false {
+                // 비콘과의 연결이 처음 감지되었거나, 다시 연결된 경우
+                ServerCommunicator.sendBeaconDataToServer(beacon: beacon)
+                beaconConnectedStatus[beaconKey] = true
+            }
+
+            // 연결 끊김 타이머 리셋
+            resetDisconnectTimer(beaconKey: beaconKey)
+
+            // 마지막으로 비콘을 감지한 시간 업데이트
+            lastSeenBeacons[beaconKey] = Date()
+        }
+
+        func handleDisconnectedBeacon(beaconKey: String) {
+            // 연결 끊김 상태를 처리하는 로직, 1분 후에 "연결 끊김" 메시지 전송
+            if let lastSeenTime = lastSeenBeacons[beaconKey], -lastSeenTime.timeIntervalSinceNow >= 60 {
+                if beaconConnectedStatus[beaconKey] ?? false {
+                    ServerCommunicator.sendBeaconDataToServer() // "연결 끊김" 메시지 전송
+                    beaconConnectedStatus[beaconKey] = false // 상태 업데이트
+                }
+            }
+        }
+
+        func resetDisconnectTimer(beaconKey: String) {
+            disconnectTimers[beaconKey]?.invalidate()
+            disconnectTimers[beaconKey] = Timer.scheduledTimer(withTimeInterval: 60, repeats: false) { [weak self] _ in
+                self?.handleDisconnectedBeacon(beaconKey: beaconKey)
+                self?.disconnectTimers[beaconKey] = nil
+            }
+        }
     
     func updateDistance(_ beacons: [CLBeacon]) {
         
@@ -101,7 +141,7 @@ class BeaconDebugger: BeaconBase {
     func notifyServerAboutBeaconConnect(beaconKey: String) {
         print("\(beaconKey) 연결됨")
         // 서버로 "연결됨" 상태를 전송하는 코드 구현
-        // ServerCommunicator.sendBeaconDataToServer(beacon: beacon) 형태로 조정할 수 있음
+        // ServerCommunicator.sendBeaconDataToServer()// 형태로 조정할 수 있음
     }
 
         
